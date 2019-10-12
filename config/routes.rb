2323de3c2d295164740012c0870db0328c32c5a5
juanesgh <jerodriguez@miuandes.cl>
2019-10-12 13:16:35 -0300

    Generate player automated

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :game_items
   resources :games
   resources :players
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations'}
   resources :items
   root to: "home#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
