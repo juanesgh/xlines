8902658d92b50a0f72dbd471092e45cf684ca8be
juanesgh <jerodriguez@miuandes.cl>
2019-10-15 01:52:34 -0300

    Change to heroku2

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,7 +2,7 @@
 
 class Users::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
-  #skip_before_action :verify_authenticity_token
+  skip_before_action :authenticate_user!
 
   # Be sure to enable JSON.
   #respond_to :html, :json
