485dfb3458d77edb91393710366f710f57a90bb5
juanesgh <jerodriguez@miuandes.cl>
2019-10-15 02:00:12 -0300

    Change to heroku2

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class Users::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
-  skip_before_action :authenticate_user!
+  before_action :configure_sign_in_params, only: [:create]
+  #skip_before_action :authenticate_user!
 
   # Be sure to enable JSON.
   #respond_to :html, :json
